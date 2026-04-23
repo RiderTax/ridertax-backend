b0fc79f84506bea3010650e394631faa2a19660e
RiderTax <nikaat232@gmail.com>
2026-04-23 11:15:37 +0530

    Update test-fraud.js

--- api/hmrc/test-fraud.js
+++ api/hmrc/test-fraud.js
@@ -1,1 +1,23 @@
+import axios from "axios";
+import { buildFraudHeaders } from "../../utils/hmrcFraudHeaders";
 
+export default async function handler(req, res) {
+  try {
+    const user_id = req.query.user_id || "test-user";
+
+    const headers = buildFraudHeaders(req, user_id);
+
+    const response = await axios.get(
+      "https://test-api.service.hmrc.gov.uk/test/fraud-prevention-headers/validate",
+      { headers }
+    );
+
+    return res.status(200).json(response.data);
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+
+    return res.status(500).json({
+      error: err.response?.data || err.message,
+    });
+  }
+}
